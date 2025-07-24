0fddc72162a6d57658d7619a67ead96fe0754afb
tadams048 <tadams048@gmail.com>
2025-07-24 09:57:53 -0700

    Initial Commit

---
+++ Socra/SocraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocraApp.swift
+//  Socra
+//
+//  Created by Taylor Adams on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SocraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SocraTests/SocraTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocraTests.swift
+//  SocraTests
+//
+//  Created by Taylor Adams on 7/24/25.
+//
+
+import Testing
+@testable import Socra
+
+struct SocraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
